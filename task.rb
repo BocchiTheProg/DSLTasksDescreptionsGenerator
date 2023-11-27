966e8ace32e7a6324276a7ac7c3d52afda7792cd
BocchiTheProg <bocchitheprog@gmail.com>
2023-11-27 12:27:05 +0200

    Created Task class (represents description of a specific task).

---
+++ task.rb
@@ -0,0 +1,27 @@
+class Task
+  attr_accessor :name, :description, :priority, :due_date, :executors
+
+  def initialize(name)
+    @name = name
+    @description = ''
+    @priority = 0
+    @due_date = ''
+    @executors = []
+  end
+
+  def add_description(description)
+    @description = description
+  end
+
+  def add_priority(priority)
+    @priority = priority
+  end
+
+  def add_due_date(due_date)
+    @due_date = due_date
+  end
+
+  def add_executors(executors)
+    @executors = executors
+  end
+end
