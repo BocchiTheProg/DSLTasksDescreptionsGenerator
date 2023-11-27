f8109b79cd0be33f0da38a855a55abc50e62a032
BocchiTheProg <bocchitheprog@gmail.com>
2023-11-27 15:48:42 +0200

    Added check when selecting the task, added rescue to handle NoMethodError (when no task was selected).

--- project.rb
+++ project.rb
@@ -69,41 +69,56 @@ class Project
   end
 
   def execute_command(command)
-    case command
-    when /^CREATE\("(.+)"\)$/
-      if select_task($1).nil?
-        create_task($1)
-      else
-        puts "Task with name #{$1} already exist."
-        if ask_yn_question("Do you want to recreate this Task?")
-          delete_task($1)
+    begin
+      case command
+      when /^CREATE\("(.+)"\)$/
+        if select_task($1).nil?
           create_task($1)
+        else
+          puts "Task with name #{$1} already exist."
+          if ask_yn_question("Do you want to recreate this Task?")
+            delete_task($1)
+            create_task($1)
+          end
         end
+      when /^DELETE\("(.+)"\)$/
+        delete_task($1)
+      when /^SELECT\("(.+)"\)$/
+        if (@current_task = select_task($1)).nil?
+          puts "No Task was selected (cannot find Task with name #{$1}.)"
+          puts "List of task names in project:"
+          @tasks.each { |task| puts task.name}
+        else
+          puts "Task: #{$1} was selected."
+        end
+      when /^DESCRIPTION\("(.+)"\)$/
+        @current_task.add_description($1)
+      when /^PRIORITY\((\d+)\)$/
+        @current_task.add_priority($1.to_i)
+      when /^DUE_DATE\("(.+)"\)$/
+        @current_task.add_due_date($1)
+      when /^EXECUTORS\((.+)\)$/
+        @current_task.add_executors($1.split(','))
+      when /^SORT_BY_PRIORITY$/
+        sort_by_priority
+      when /^SORT_BY_DUE_DATE$/
+        sort_by_due_date
+      when /^SAVE_TO_FILE\("(.+)"\)$/
+        save_to_file($1)
+      when /^LOAD_FROM_FILE\("(.+)"\)$/
+        load_from_file($1)
+      when /^QUIT$/
+        exit
+      else
+        puts "Invalid command"
       end
-    when /^DELETE\("(.+)"\)$/
-      delete_task($1)
-    when /^SELECT\("(.+)"\)$/
-      @current_task = select_task($1)
-    when /^DESCRIPTION\("(.+)"\)$/
-      @current_task.add_description($1)
-    when /^PRIORITY\((\d+)\)$/
-      @current_task.add_priority($1.to_i)
-    when /^DUE_DATE\("(.+)"\)$/
-      @current_task.add_due_date($1)
-    when /^EXECUTORS\((.+)\)$/
-      @current_task.add_executors($1.split(','))
-    when /^SORT_BY_PRIORITY$/
-      sort_by_priority
-    when /^SORT_BY_DUE_DATE$/
-      sort_by_due_date
-    when /^SAVE_TO_FILE\("(.+)"\)$/
-      save_to_file($1)
-    when /^LOAD_FROM_FILE\("(.+)"\)$/
-      load_from_file($1)
-    when /^QUIT$/
-      exit
-    else
-      puts "Invalid command"
+    rescue NoMethodError => e
+      if @current_task.nil?
+        puts "Cant add descriptions, no Task was selected."
+        puts "Use SELECT operator."
+      else
+        puts "Error #{e.message}"
+      end
     end
   end
 
